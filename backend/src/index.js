b2ad871f064898271af359c70f861a2e9683bc8b
Akila Wijerathna <akilawijerathna5@gmail.com>
2024-12-19 13:40:00 +0530

    Fix some small errors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
 import {connectDB} from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
@@ -11,7 +11,7 @@ const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, ()=> {
+app.listen(PORT, () => {
     console.log("Server is running on PORT:" + PORT);
-    connectDB()
+    connectDB();
 });
